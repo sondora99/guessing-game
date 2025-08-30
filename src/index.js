36bef3263b30b209071627dda8cf607378ab506d
sondora99 <marymikser@gmail.com>
2025-08-30 20:42:33 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.mid = 0;
+    this.cur = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.mid = Math.round((this.max + this.min) / 2);
+    this.cur = this.mid;
+    return this.cur;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.cur;
+    this.mid = Math.round(this.cur + this.min / 2);
+    this.cur = this.mid;
+    return this.cur;
+  }
+
+  greater() {
+    this.min = this.cur;
+    this.mid = Math.round(this.max + this.cur / 2);
+    this.cur = this.mid;
+    return this.cur;
+  }
 }
 
 module.exports = GuessingGame;
